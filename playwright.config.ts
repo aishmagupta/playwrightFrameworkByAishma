9457a57b304e003466b1ea599ace7ea3463726d1
aishma.gupta <aishma.gupta@simpplr.com>
2026-04-25 19:14:36 +0530

    fix: update headed

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,13 +5,13 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 4,
   reporter: 'html',
   use: {
     baseURL: 'https://www.saucedemo.com',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
-    headless: process.env.CI === 'true' ? false : true,
+    headless: !!process.env.CI
   },
   projects: [{ name: 'chromium', use: { ...devices['Desktop Chrome'] } }],
   testMatch: /.*\.spec\.ts/,
